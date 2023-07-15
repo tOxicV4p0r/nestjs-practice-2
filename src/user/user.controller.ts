7e47b480edda944b2978062692e0209753cd07f3
kraisorn <22193826+tOxicV4p0r@users.noreply.github.com>
2023-07-15 23:07:07 +0700

    create custom Guard to avoid bug

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,10 +1,11 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AuthGuard } from "@nestjs/passport";
+import { JwtGuard } from 'src/auth/guard';
 
 @Controller('users')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt2'))
+    @UseGuards(JwtGuard)
     @Get('me')
     getMe() {
         return { me: '' };
